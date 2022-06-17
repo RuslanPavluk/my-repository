a257571663ee754b4e1d5f85f58f9e279deb8748
RuslanPavluk <104434624+RuslanPavluk@users.noreply.github.com>
2022-06-17 23:35:09 +0300

    Add files via upload

--- дз по 5 лекции/main.go
+++ дз по 5 лекции/main.go
@@ -3,35 +3,37 @@ package main
 import "fmt"
 
 const (
-	priceApple float32 = 5.99
-	pricePear  float32  = 7.0
-	myMoney    float32  = 26.0
+	priceApple float64 = 5.99
+	pricePear  float64  = 7.0
+	myMoney    float64  = 26.0
 )
 
 func main() {
 	amountApple:= 9 
-	applesCost1:=float32(priceApple)*float32(amountApple)
+	applesCost:=priceApple*float64(amountApple)
 	
 	amountPear:= 8 
-	pearsCost1:=float32(pricePear)*float32(amountPear)
+	pearsCost:=pricePear*float64(amountPear)
 
-	fmt.Println("стоимость заданного количества яблок -", applesCost1)
-	fmt.Println("стоимость заданного количества груш -", pearsCost1)
+	fmt.Println("стоимость заданного количества яблок -", applesCost)
+	fmt.Println("стоимость заданного количества груш -", pearsCost)
 	
-	fmt.Println("сколько нужно потратить денег, чтобы купить заданное количество яблок и груш?-", applesCost1+pearsCost1)
+	fmt.Println("сколько нужно потратить денег, чтобы купить заданное количество яблок и груш?-", applesCost+pearsCost)
 	
-	fmt.Println("сколько груш мы можем купить?-", myMoney/pricePear)
-	
-	fmt.Println("сколько яблок мы можем купить?-", myMoney/priceApple)
+	money:=26
+	costOnePear:=7
+	costOneApple:=5.99
+	fmt.Println("сколько груш мы можем купить?-", money/costOnePear)
+	fmt.Println("сколько яблок мы можем купить?-", money/int(costOneApple))
 
 	amountApple= 2
-	applesCost2:=float32(priceApple)*float32(amountApple)
+	applesCost=priceApple*float64(amountApple)
 	amountPear=2 
-	pearsCost2 := float32(pricePear) * float32(amountPear)
-	fmt.Println("сколько стоит заданное количество яблок?-", applesCost2)
-	fmt.Println("сколько стоит заданное количество груш?-", pearsCost2)
+	pearsCost=pricePear * float64(amountPear)
+	fmt.Println("сколько стоит заданное количество яблок?-", applesCost)
+	fmt.Println("сколько стоит заданное количество груш?-", pearsCost)
 
-	sum :=applesCost2 + pearsCost2
+	sum :=applesCost + pearsCost
 	ourPurchase := myMoney >= sum
 	fmt.Println("можем ли мы купить заданное количество яблок и груш?-", ourPurchase)
  }

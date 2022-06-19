c7bc1a58a10bad6ea704c276f7ce82f09f37f0de
RuslanPavluk <104434624+RuslanPavluk@users.noreply.github.com>
2022-06-19 22:37:51 +0300

    Add files via upload

--- дз по 5 лекции/main.go
+++ дз по 5 лекции/main.go
@@ -4,36 +4,35 @@ import "fmt"
 
 const (
 	priceApple float64 = 5.99
-	pricePear  float64  = 7.0
-	myMoney    float64  = 26.0
+	pricePear  float64 = 7.0
+	myMoney    float64 = 26.0
 )
 
 func main() {
-	amountApple:= 9 
-	applesCost:=priceApple*float64(amountApple)
-	
-	amountPear:= 8 
-	pearsCost:=pricePear*float64(amountPear)
+	amountApple := 9
+	applesCost := priceApple * float64(amountApple)
+
+	amountPear := 8
+	pearsCost := pricePear * float64(amountPear)
 
 	fmt.Println("стоимость заданного количества яблок -", applesCost)
 	fmt.Println("стоимость заданного количества груш -", pearsCost)
-	
-	fmt.Println("сколько нужно потратить денег, чтобы купить заданное количество яблок и груш?-", applesCost+pearsCost)
-	
-	money:=26
-	costOnePear:=7
-	costOneApple:=5.99
-	fmt.Println("сколько груш мы можем купить?-", money/costOnePear)
-	fmt.Println("сколько яблок мы можем купить?-", money/int(costOneApple))
 
-	amountApple= 2
-	applesCost=priceApple*float64(amountApple)
-	amountPear=2 
-	pearsCost=pricePear * float64(amountPear)
+	fmt.Println("сколько нужно потратить денег, чтобы купить заданное количество яблок и груш?-", applesCost+pearsCost)
+
+	applesCanBuy := myMoney / priceApple
+	pearsCanBuy := myMoney / pricePear
+	fmt.Println("сколько груш мы можем купить?-", int(applesCanBuy))
+	fmt.Println("сколько яблок мы можем купить?-", int(pearsCanBuy))
+
+	amountApple = 2
+	applesCost = priceApple * float64(amountApple)
+	amountPear = 2
+	pearsCost = pricePear * float64(amountPear)
 	fmt.Println("сколько стоит заданное количество яблок?-", applesCost)
 	fmt.Println("сколько стоит заданное количество груш?-", pearsCost)
 
-	sum :=applesCost + pearsCost
+	sum := applesCost + pearsCost
 	ourPurchase := myMoney >= sum
 	fmt.Println("можем ли мы купить заданное количество яблок и груш?-", ourPurchase)
- }
+}
